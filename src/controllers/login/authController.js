2db7aff863aa99e1bf97dc4ca6fd33a63edb1605
Luis Gontarski <luisedgon@outlook.com>
2025-05-21 22:14:45 -0300

    atualizado crud login

--- src/controllers/login/authController.js
+++ src/controllers/login/authController.js
@@ -20,3 +20,43 @@ exports.login = (req, res) => {
 
   res.json({ token });
 };
+
+exports.getUserById = (req, res) => {
+  const { id } = req.params;
+
+  if (id != fakeUser.id) {
+    return res.status(404).json({ message: 'Usuário não encontrado' });
+  }
+
+  res.json({ id: fakeUser.id, email: fakeUser.email });
+}
+
+exports.create = (req, res) => {
+  const { email, senha } = req.body;
+
+  if (!email || !senha) {
+    return res.status(400).json({ message: 'Email e senha são obrigatórios' });
+  }
+
+  res.status(201).json({ message: 'Usuário registrado com sucesso!' });
+};
+
+exports.update = (req, res) => {
+  const { email, senha } = req.body;
+
+  if (!email || !senha) {
+    return res.status(400).json({ message: 'Email e senha são obrigatórios' });
+  }
+
+  res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
+};
+
+exports.delete = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ message: 'ID do usuário é obrigatório' });
+  }
+
+  res.status(200).json({ message: 'Usuário deletado com sucesso!' });
+}
--- src/routes/login/auth.js
+++ src/routes/login/auth.js
@@ -4,5 +4,9 @@ const authController = require('../../controllers/login/authController');
 const authenticate = ('../middleware/authenticate');
 
 router.post('/login', authController.login);
+router.get('/user', authController.getUserById)
+router.post('/create', authController.create);
+router.put('/update', authController.update);
+router.delete('/delete', authController.delete);
 
 module.exports = router;
