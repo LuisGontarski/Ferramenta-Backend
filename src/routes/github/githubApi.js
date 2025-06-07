9a07e9778c73cafc606959d44a79317c3674df4c
Luis Gontarski <83317182+LuisGontarski@users.noreply.github.com>
2025-06-07 05:57:38 -0300

    att

--- src/routes/github/githubApi.js
+++ src/routes/github/githubApi.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
 const commitCountController = require("../../controllers/github/commitCountController");
 const githubAuthController = require("../../controllers/github/githubAuthController");
 
 router.get("/github/commit/count", commitCountController.getCommitCount);
 
-router.get("/auth/github/callback", githubAuthController.getGithubCallback);
-router.get("/auth/github/login", githubController.githubLogin);
+router.get("/auth/github/callback", githubAuthController.githubCallback);
+router.get("/auth/github/login", githubAuthController.githubLogin);
 
 module.exports = router;
